0ffa82924f43e421b8549253c5d2d91e5c079424
Tabish42 <tabishsheikh42@gmail.com>
2023-09-22 14:27:03 +0530

    second version

--- script.js
+++ script.js
@@ -24,31 +24,31 @@ var d = document.querySelectorAll("button").length;
 
      switch(key) {
          case "w":
-             var tom_1 = new Audio("sounds/tom-1.mp3");
+             var tom_1 = new Audio("public/sounds/tom-1.mp3");
              tom_1.play();
              break;
          case "a":
-             var tom2 = new Audio("sounds/tom-2.mp3");
+             var tom2 = new Audio("public/sounds/tom-2.mp3");
              tom2.play();
              break;
          case "s":
-             var audio = new Audio("sounds/tom-3.mp3");
+             var audio = new Audio("public/sounds/tom-3.mp3");
              audio.play();
              break;
          case "d":
-             var audio = new Audio("sounds/tom-4.mp3");
+             var audio = new Audio("public/sounds/tom-4.mp3");
              audio.play();
              break;
          case "j":
-             var audio = new Audio("sounds/kick-bass.mp3");
+             var audio = new Audio("public/sounds/kick-bass.mp3");
              audio.play();
              break;
          case "k":
-             var audio = new Audio("sounds/snare.mp3");
+             var audio = new Audio("public/sounds/snare.mp3");
              audio.play();
              break;
          case "l":
-             var audio = new Audio("sounds/crash.mp3");
+             var audio = new Audio("public/sounds/crash.mp3");
              audio.play();
              break;
          default:
