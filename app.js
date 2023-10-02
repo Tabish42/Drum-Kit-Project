fbfcebc0b005776673d23c2874898ca58962661f
Tabish42 <tabishsheikh42@gmail.com>
2023-10-02 15:00:47 +0530

    Made few Changes

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log("Server is running at port 8080");
 });
